200618d0d3822ac39537f1e3d82dfbc58f5b0a54
ayanstack94b <ayondevs.94b@gmail.com>
2025-03-01 17:51:35 +0530

    js added alert added

---
+++ Script/script.js
@@ -0,0 +1,9 @@
+let buttons = document.querySelectorAll('.complete-btn');
+const 
+
+for (const button of buttons) {
+    button.addEventListener('click', function () {
+        alert('Board Update Successfully');
+
+    })
+}
