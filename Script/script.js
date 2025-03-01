0d32a60c491a6117a8793cefd3fd43e3b4d538cf
ayanstack94b <ayondevs.94b@gmail.com>
2025-03-01 23:32:05 +0530

    dynamic js added

--- Script/script.js
+++ Script/script.js
@@ -1,6 +1,9 @@
 let buttons = document.querySelectorAll('.complete-btn');
 let taskNumberElement = document.getElementById('task-number');
 let completedTasksElement = document.getElementById('completed-tasks');
+let newElement = document.getElementById('displayed-element');
+
+
 
 for (let button of buttons) {
     button.addEventListener('click', function () {
@@ -20,13 +23,25 @@ for (let button of buttons) {
             taskNumberElement.innerText = convertedTaskNum;
             completedTasksElement.innerText = convertedCompletedTasks;
         }
+
         else if (convertedTaskNum === 1) {
             convertedTaskNum--;
             convertedCompletedTasks++;
             taskNumberElement.innerText = convertedTaskNum;
             completedTasksElement.innerText = convertedCompletedTasks;
 
-            alert('No tasks left!');
+            alert('congrats!!! You have completed all the current tasks');
+
+        }
+
+
+        let title = document.querySelector('.title').innerText
+
+        if (newElement) {
+            const p = document.createElement('p');
+            p.classList.add('dom-style');
+            p.innerText = `You have completed the task: ${title}`;
+            newElement.appendChild(p);
         }
     });
 }
@@ -37,7 +52,27 @@ document.getElementById('to-blog-page').addEventListener('click', function () {
     window.location.href = 'Blogs.html';
 })
 
+// Random Color Button
+document.getElementById('change-clr-btn').addEventListener('click', function () {
+    let randomNum = Math.floor(Math.random() * 16777215);
+    randomNum = '#' + randomNum.toString(16);
+    document.body.style.backgroundColor = randomNum;
+})
+// set current time and date
+function updateDate() {
+    let now = new Date();
+    let formattedDate = now.toLocaleDateString('en-US', {
+        weekday: 'long',
+        day: '2-digit',
+        month: '2-digit',
+        year: 'numeric'
+    });
 
+    let dateElement = document.getElementById('date');
+    dateElement.innerText = formattedDate;
+    dateElement.style.fontWeight = "bold";
+}
 
+updateDate();
 
 
