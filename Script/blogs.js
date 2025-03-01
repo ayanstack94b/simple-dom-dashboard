2b0654af6f40a473d24bb8699e7940ced2edca4a
ayanstack94b <ayondevs.94b@gmail.com>
2025-03-01 21:13:03 +0530

    both blog and index page are connected

---
+++ Script/blogs.js
@@ -0,0 +1,4 @@
+document.getElementById('home-page-btn').addEventListener('click', function () {
+    window.location.href = 'index.html';
+    console.log('hello')
+})
--- Script/script.js
+++ Script/script.js
@@ -1,9 +1,43 @@
 let buttons = document.querySelectorAll('.complete-btn');
-const 
+let taskNumberElement = document.getElementById('task-number');
+let completedTasksElement = document.getElementById('completed-tasks');
 
-for (const button of buttons) {
+for (let button of buttons) {
     button.addEventListener('click', function () {
         alert('Board Update Successfully');
 
-    })
+        let convertedTaskNum = parseInt(taskNumberElement.innerText);
+        let convertedCompletedTasks = parseInt(completedTasksElement.innerText);
+
+
+        button.disabled = true;
+        button.style.backgroundColor = "#ccc";
+        button.style.cursor = "not-allowed";
+
+        if (convertedTaskNum > 1) {
+            convertedTaskNum--;
+            convertedCompletedTasks++;
+            taskNumberElement.innerText = convertedTaskNum;
+            completedTasksElement.innerText = convertedCompletedTasks;
+        }
+        else if (convertedTaskNum === 1) {
+            convertedTaskNum--;
+            convertedCompletedTasks++;
+            taskNumberElement.innerText = convertedTaskNum;
+            completedTasksElement.innerText = convertedCompletedTasks;
+
+            alert('No tasks left!');
+        }
+    });
 }
+
+// Blogs page button
+
+document.getElementById('to-blog-page').addEventListener('click', function () {
+    window.location.href = 'Blogs.html';
+})
+
+
+
+
+
